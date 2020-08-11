c537c555a17afcccd142eadd723f29bd280f29ba
abel <abel@nomail.com>
2020-08-11 11:59:13 +0000

    No need for joypixels if it is configured correctly in fontconfg.fonts

--- config.def.h
+++ config.def.h
@@ -8,8 +8,7 @@
 static char *font = "mono:pixelsize=15:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    "Hack Nerd Font:pixelsize=15:antialias=true:autohint=true", // Powerline
-    "Joy Pixels:pixelsize=15:antialias=true:autohint=true" // Unicode
+    "Hack Nerd Font:pixelsize=15:antialias=true:autohint=true"
 };
 
 static int borderpx = 2;
